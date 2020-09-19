5a85a581b247915d8c11e03b1b9377a84b5bedc1
EasterFan <fan.easter@gmail.com>
2020-09-19 22:30:09 +0800

    start application success

--- foodie-api/src/main/java/FoodieApplication.java
+++
@@ -1,7 +0,0 @@
-/**
- * @Author EasterFan working on 2020/9/19 9:54 下午
- * @Description 启动类
- * @Version 1.0
- */
-public class FoodieApplication {
-}
---
+++ foodie-api/src/main/java/com/easter/FoodieApplication.java
@@ -0,0 +1,17 @@
+package com.easter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author EasterFan working on 2020/9/19 9:54 下午
+ * @Description 启动类
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class FoodieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieApplication.class, args);
+    }
+}
